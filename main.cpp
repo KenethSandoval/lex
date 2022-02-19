e86d15e2549e14f96b3bb36a83ab75b43abc2189
KenethSandoval <kenethra.74@gmail.com>
2022-02-18 23:33:10 -0600

    feat: add signal system

--- main.cpp
+++ main.cpp
@@ -1,98 +1,27 @@
 #include <X11/Xlib.h>
-#include <X11/Xutil.h>
-#include <X11/Xos.h>
+#include <X11/XF86keysym.h>
+#include <X11/XKBlib.h>
 
-#include <cstdio>
 #include <iostream>
-#include <cstring>
+#include <csignal>
+#include <unistd.h>
 #include <memory>
 #include <functional>
-#include <sys/utsname.h>
+
+#include "lex.hpp"
+
+static client 		*list = {0}, *ws_list[10] = {0}, *cur;
+static int 		ws = 1, sw, sh, wx, wy, numlock = 0;
+static unsigned int 	ww, wh;
+
+auto d = std::unique_ptr<Display, std::function<void(Display*)>> (XOpenDisplay(0), XFlush);
+static XButtonEvent 	mouse;
+static Window 		root;
 
 auto main() -> int {
-  //initialize Display connection with a smart pointer
-  auto dpy = std::unique_ptr<Display, std::function<void(Display*)>>(
-      XOpenDisplay(0), XFlush);
+	XEvent ev;
 
-  if (dpy.get() == NULL) {
-	std::cout << "Cannot open display" << std::endl;
-	exit(1);
-  }
+	if(!(d.get())) exit (1);
 
-  //Create some colors
-  int blackColor = BlackPixel(dpy.get(), DefaultScreen(dpy.get()));
-  int whiteColor = WhitePixel(dpy.get(), DefaultScreen(dpy.get()));
-
-  int s = DefaultScreen(dpy.get());
-  //initialize window
-  Window w = XCreateSimpleWindow(dpy.get(), RootWindow(dpy.get(), s), 10, 10, 660, 200, 1, blackColor, whiteColor);
-
-
-  //Call for map notify events
-  XSelectInput(dpy.get(), w, ExposureMask | KeyPressMask);
-
-  //show window on screen
-  XMapWindow(dpy.get(), w);
-
-  XStoreName(dpy.get(), w, "Lex");
-
-  Atom WM_DELETE_WINDOW = XInternAtom(dpy.get(), "WM_DELETE_WINDOW", False);
-  XSetWMProtocols(dpy.get(), w, &WM_DELETE_WINDOW, 1);
-
-  bool uname_ok = false;
-  struct utsname sname;
-  int ret = uname(&sname);
-
-  if (ret != -1) {
-	uname_ok = true;
-  }
-
-  XEvent e;
-
-  while(1) {
-	XNextEvent(dpy.get(), &e);
-	if (e.type == Expose) {
-		int y_offset = 20;
-		const char* s1 = "X11 test app under Linux";
-
-		const char* s2 = "Lex";
-		XDrawString(dpy.get(), w, DefaultGC(dpy.get(), s), 10, y_offset, s1, strlen(s1));
-		XDrawString(dpy.get(), w, DefaultGC(dpy.get(), s), 10, y_offset, s2, strlen(s2));
-		y_offset += 20;
-
-		if (uname_ok) {
-			char buf[256] = {0};
-
-			sprintf(buf, "System information: ");
-			XDrawString(dpy.get(), w, DefaultGC(dpy.get(), s), 10, y_offset, buf, strlen(buf));
-			y_offset += 15;
-		}
-
-		XWindowAttributes wa;
-		XGetWindowAttributes(dpy.get(), w, &wa);
-		int width = wa.width;
-		int height = wa.height;
-		char buf[128] = {0};
-		XDrawString(dpy.get(), w, DefaultGC(dpy.get(), s), 10, y_offset, buf, strlen(buf));
-		y_offset += 20;
-	}
-
-	if (e.type == KeyPress) {
-		char buf[128] = {0};
-		KeySym keysym;
-		int len = XLookupString(&e.xkey, buf, sizeof buf, &keysym, NULL);
-		if(keysym == XK_Escape) {
-			break;
-		}
-	}
-	if ((e.type == ClientMessage) &&
-			(static_cast<unsigned int>(e.xclient.data.l[0]) == WM_DELETE_WINDOW)) {
-		break;
-	}
-  }
-
-  XDestroyWindow(dpy.get(), w);
-  XCloseDisplay(dpy.get());
-
-  return 0;
+	signal(SIGCHLD, SIG_IGN);
 }
