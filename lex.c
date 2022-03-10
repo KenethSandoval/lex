ebb027811345cc96252e560b7f727dbf74104484
KenethSandoval <kenethra.74@gmail.com>
2022-03-09 19:16:17 -0600

    feat: windel and execute terminal

--- lex.c
+++ lex.c
@@ -106,7 +106,19 @@ void wind_add(Window w) {
 	ws_save(ws);
 }
 
-void win_del(Window w) {}
+void win_del(Window w) {
+	client *x = 0;
+
+	for win if (c->w == w) x = c;
+
+	if (x->prev == x) list = 0;
+	if (list == x) 		list = x->next;
+	if(x->next) 			x->next->prev = x->prev;
+	if(x->prev)				x->prev->next = x->next;
+
+	free(x);
+	ws_save(ws);
+}
 
 void win_kill(const Arg arg){
 	if (cur) XKillClient(d, cur->w);
@@ -119,15 +131,66 @@ void win_center(const Arg arg) {
 	XMoveWindow(d, cur->w, (sw - ww) / 2, (sh - wh)/2);
 }
 
-void win_fs(const Arg arg) {}
+void win_fs(const Arg arg) {
+	if (!cur) return;
 
-void win_to_ws(const Arg arg) {}
+	if ((cur->f = cur->f ? 0 : 1)) {
+		win_size(cur->w, &(int){0}, &(int){0}, &ww, &wh);
+		XMoveWindow(d, cur->w, (sw - ww) / 2, (sh - wh) / 2);
+	} else {
+		XMoveResizeWindow(d, cur->w, cur->wx, cur->wy, cur->ww, cur->wh);
+	}
+}
 
-void win_prev(const Arg arg) {}
+void win_to_ws(const Arg arg) {
+	int tmp = ws;
 
-void win_next(const Arg arg) {}
+	if(arg.i == tmp) return;
 
-void ws_go(const Arg arg) {}
+	ws_sel(arg.i);
+	win_add(cur->w);
+	ws_save(arg.i);
+
+	ws_sel(tmp);
+	win_del(cur->w);
+	XUnmapWindow(d, cur->w);
+	ws_save(tmp);
+
+	if (list) win_focus(list);
+}
+
+void win_prev(const Arg arg) {
+	if (!cur) return;
+
+	XRaiseWindow(d, cur->prev->w);
+	win_focus(cur->prev);
+}
+
+void win_next(const Arg arg) {
+	if (!cur) return;
+
+	XRaiseWindow(d, cur->next->w);
+	win_focus(cur->next);
+}
+
+void ws_go(const Arg arg) {
+	int tmp = ws;
+
+	if(arg.i == ws) return;
+
+	ws_save(ws);
+	ws_sel(arg.i);
+
+	for win XMapWindow(d, c->w);
+
+	ws_sel(tmp);
+
+	for win XUnmapWindow(d, c->w);
+
+	ws_sel(arg.i);
+
+	if (list) win_focus(list); else cur = 0;
+}
 
 void configure_request(XEvent *e) {
 	XConfigureRequestEvent *ev = &e->xconfigurerequest;
@@ -142,7 +205,19 @@ void configure_request(XEvent *e) {
 			});
 }
 
-void map_request(XEvent *e) {}
+void map_request(XEvent *e) {
+	Window w = e->xmaprequest.window;
+
+	XSelectInput(d, w, StructureNotifyMask|EnterWindowMask);
+	win_size(w, &wx, &wy, &ww, &wh);
+	win_add(w);
+	cur = list->prev;
+
+	if (wx + wy == 0) win_center((Arg){0});
+
+	XMapWindow(d, w);
+	win_focus(list->prev);
+}
 
 void mapping_notify(XEvent *e) {
 	XMappingEvent *ev = &e->xmapping;
