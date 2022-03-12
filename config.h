44948b2d3dd25ef9077960e65a9f238fdd114854
KenethSandoval <kenethra.74@gmail.com>
2022-03-11 19:46:12 -0600

    feat: configuration browser and terminal

--- config.h
+++ config.h
@@ -3,15 +3,20 @@
 
 #define MOD Mod4Mask
 
-const char* menu[] = {"rofi --show", 0};
-const char* term[] = {"alacritty"};
+const char* menu[] = {"rofi -show run", 0};
+const char* term[] = {"alacritty", 0};
+const char* brow[] = {"firefox", 0};
 
 static struct key keys[] = {
 	{MOD, XK_q, win_kill, {0}},
+	{MOD, XK_c, win_center, {0}},
 	{MOD, XK_f, win_fs,   {0}},
 
 	{MOD, XK_d, run, {.com=menu}},
 	{MOD, XK_Return, run, {.com=term}},
+	{MOD, XK_b, run, {.com=brow}},
+
+	//TODO: agregar workspace
 };
 
 #endif
